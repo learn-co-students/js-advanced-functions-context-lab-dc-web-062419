22605acf51f3d04d8d0b12f76147c09964dc22ca
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-13 12:16:06 -0400

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,82 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+function createEmployeeRecord(employee) {
+  return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+function createEmployees(employees) {
+  return employees.map(createEmployeeRecord);
+}
 
-    return payable
+function createTimeInEvent(timeIn) {
+  const clockEvent = {
+    type: 'TimeIn',
+    hour: parseInt(timeIn.split(' ')[1], 10),
+    date: timeIn.split(' ')[0],
+  };
+  this.timeInEvents.push(clockEvent);
+  return this;
+}
+
+function createTimeOutEvent(timeOut) {
+  const clockEvent = {
+    type: 'TimeOut',
+    hour: parseInt(timeOut.split(' ')[1]),
+    date: timeOut.split(' ')[0],
+  };
+  this.timeOutEvents.push(clockEvent);
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.filter(
+    clockEvent => clockEvent.date === date,
+  )[0].hour;
+
+  const timeOut = this.timeOutEvents.filter(
+    clockEvent => clockEvent.date === date,
+  )[0].hour;
+  return (timeOut - timeIn) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date);
+  return hoursWorked * this.payPerHour;
+}
+
+let allWagesFor = function() {
+  let eligibleDates = this.timeInEvents.map(function(e) {
+    return e.date;
+  });
+
+  let payable = eligibleDates.reduce(
+    function(memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0,
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+  return payable;
+};
+
+function calculatePayroll(employees) {
+  return employees
+    .map(employee => allWagesFor.call(employee))
+    .reduce((a, b) => a + b, 0);
+}
+
+function createEmployeeRecords(employees) {
+  return employees.map(employee => createEmployeeRecord(employee));
+}
+
+function findEmployeebyFirstName(employees, name) {
+  return employees.find(employee => employee.firstName === name);
 }

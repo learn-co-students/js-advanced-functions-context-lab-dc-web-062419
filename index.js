bcf4141a87b88170d67f88172259f2d0a389f248
Trevahr Robertson <Trevahr@gmail.com>
2019-08-22 13:47:12 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,88 @@ let allWagesFor = function () {
 
     return payable
 }
+
+
+
+
+// Your code here
+
+function createEmployeeRecords(arr){
+   return arr.map(worker => createEmployeeRecord(worker))
+}
+function createEmployeeRecord(arr){
+    return {firstName: arr[0], familyName: arr[1], title: arr[2], 
+        payPerHour: arr[3], timeInEvents: [], timeOutEvents: [], } 
+}
+
+function createEmployees(arr){
+   return arr.map( employee => createEmployeeRecord(employee))
+}
+
+ let createTimeInEvent = function (timestamp){
+      let  [date1, time1] = timestamp.split(' ')
+    let timeIn = {type: 'TimeIn', hour: parseInt(time1), date: date1}
+    this.timeInEvents.push(timeIn)
+    return this
+}
+let  createTimeOutEvent = function(timestamp){
+
+    let  [date1, time1] = timestamp.split(' ')
+  let timeIn = {type: 'TimeOut', hour: parseInt(time1), date: date1}
+  this.timeOutEvents.push(timeIn)
+  return this
+}
+
+
+
+let hoursWorkedOnDate = function(timestamp){
+
+   let timein = this.timeInEvents.filter(elm => elm.date === timestamp)[0].hour
+   let timeout = this.timeOutEvents.filter(elm => elm.date === timestamp)[0].hour
+   
+
+
+    return (timeout - timein)/100
+}
+
+
+
+
+
+
+
+let wagesEarnedOnDate = function(timestamp){
+    let hoursWorked = hoursWorkedOnDate.call(this, timestamp)
+
+    return hoursWorked * this.payPerHour
+}
+
+
+
+
+
+function findEmployeebyFirstName(arr, name){
+ return arr.find(worker => worker.firstName === name) 
+}
+
+
+
+
+// function calculatePayroll(empRecords){
+//     let wagesForAll = empRecords.map(worker => worker.allWagesFor())
+//   return  wagesForAll.reduce((total, i) => total + i)
+// }
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
+
+
+
+
+
+
+
